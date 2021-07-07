074bfc40781a595b891a08477ef9cba3944560cd
Aby Kal <abyabrkal@gmail.com>
2021-07-07 15:12:02 +0400

    Dashboard Template ready to serve. Template location added to settings.py. dashboard-html file updated with static urls. urls and views updated with home view.

--- softdashdj/settings.py
+++ softdashdj/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'dash',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'softdashdj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'dash/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
