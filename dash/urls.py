0d04fa694627133acb2ac83cdd7de5396f55eaad
Aby Kal <abyabrkal@gmail.com>
2021-07-08 16:40:40 +0400

    DRY optimization on templates.

--- dash/urls.py
+++ dash/urls.py
@@ -3,5 +3,11 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('dashboard/', views.home, name='dashboard'),
+    path('billing/', views.billing, name='billing'),
+    path('profile/', views.profile, name='profile'),
+    path('signin/', views.signin, name='signin'),
+    path('signup/', views.signup, name='signup'),
+    path('tables/', views.tables, name='tables'),
+    path('vr/', views.vr, name='virtualreality'),
 ]
--- dash/views.py
+++ dash/views.py
@@ -4,3 +4,16 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'dash/dashboard.html')
+
+def billing(request):
+    return render(request, 'dash/billing.html')
+def profile(request):
+    return render(request, 'dash/profile.html')
+def signin(request):
+    return render(request, 'dash/signin.html')
+def signup(request):
+    return render(request, 'dash/signup.html')
+def tables(request):
+    return render(request, 'dash/tables.html')
+def vr(request):
+    return render(request, 'dash/virtualreality.html')
